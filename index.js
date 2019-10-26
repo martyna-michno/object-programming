4f778776c8b25449bb4fe58971fbdbc1e38fb231
Martyna Michno <mjjmichno@gmail.com>
2019-10-26 11:42:17 +0200

    some files moved to new repository

---
+++ index.js
@@ -0,0 +1,205 @@
+console.log('Hello World!')
+
+var student1_proba = {
+    name: "Martyna",
+    adress: "Sloneczna 12/4",
+    studentIdNumber: "281038",
+    profile: "Material Engineering",
+    displayFullInfo: function () {
+
+        // console.log('Imię ' + this.name + 'Adres ' + this.adress + 'Profil ' + this.studentIdNumber + 'Profil ' + this.profile);
+        console.log(`Imię: ${this.name} Adres: ${this.adress}`);
+
+    },
+    ChangeId: function (argument) {
+
+        this.studentIdNumber = argument.toString();
+
+    }
+
+}
+
+var student2_proba = {
+    name: "Jagoda",
+    adress: "Wielkokacka 13/4",
+    studentIdNumber: "950707",
+    profile: "Law",
+    displayFullInfo: function () {
+
+        console.log(this.name, this.adress, this.studentIdNumber, this.profile);
+
+    },
+    ChangeId: function (argument) {
+
+        this.studentIdNumber = argument.toString();
+
+    }
+
+}
+
+// object literal
+
+// var studentsList = [];
+
+// var student = {
+//     name: '',
+//     adress: '',
+//     studentIdNumber: '',
+//     profile: '',
+//     setData: function (name, adress, IdNumber, profile) {
+
+//         this.name = name;
+//         this.adress = adress;
+//         this.studentIdNumber = IdNumber.toString();
+//         this.profile = profile;
+
+//     },
+//     displayFullInfo: function () {
+
+
+//         console.log(`Imię: ${this.name} Adres: ${this.adress} NumerId: ${this.studentIdNumber} Profil: ${this.profile}`);
+
+//     },
+//     ChangeId: function (newId) {
+
+//         this.studentIdNumber = newId.toString();
+
+//     }
+
+// }
+
+// var student1 = Object.create(student);
+// student1.setData("Marek", "Gdańsk", 2930, "Prawo");
+
+// var student2 = Object.create(student);
+// student2.setData("Ania", "Gdynia", 4590, "Medycyna");
+
+// var student3 = Object.create(student);
+// student3.setData("Joanna", "Warszawa", 3499, "Weterynaria");
+
+// var student4 = Object.create(student);
+// student4.setData("Martyna", "Sopot", 1240, "Filologia polska");
+
+// var student5 = Object.create(student);
+// student5.setData("Michał", "Wrocław", 9732, "Ekonomia");
+
+// studentsList.push(student1, student2, student3, student4, student5);
+
+// dodawanie danych do html'a
+
+var o_cont = document.querySelector('#display');
+
+var o_html = '';
+
+studentsList.map((element) => {
+
+o_html+=`<div><div>${element.name}</div><div>${element.adress}</div> <div>${element.studentIdNumber}</div> <div>${element.profile} </div></div>`;
+
+});
+
+o_cont.innerHTML = o_html;
+
+
+//construction function
+
+// function student(name, adress, id, profile) {
+//     this.name = name;
+//     this.adress = adress;
+//     this.studentIdNumber = id;
+//     this.profile = profile;
+//     this.displayFullInfo = function () {
+
+
+//         console.log(`Imię: ${name} Adres: ${adress} NumerId: ${id} Profil: ${profile}`);
+
+//     },
+//     this.ChangeId = function (newId) {
+
+//         this.studentIdNumber = newId.toString();
+
+//     }
+
+// }
+
+
+// var studentsList = [
+
+// new student ('Martyna', 'Gdynia', '3892', "Inżynieria materiałowa"),
+// new student ('Ania', 'Gdańsk', '2342', 'Medycyna'),
+// new student ('Marta', 'Sopot', '9201', 'Kosmetologia')
+
+// ];
+
+// console.log(studentsList);
+// console.log(studentsList[1].displayFullInfo());
+// console.log(studentsList[2].ChangeId(5555));
+
+
+//zmiana funkcji w poprzednim przypadku:
+// this.ChangeId = function (newId) {
+
+//     this.studentIdNumber = newId.toString();
+
+// }
+// byloby problematyczne, bo obiekty, juz utworzone na podstawie tego wczesniejszego juz sie nie zmienia
+//dopiero te nowe sie zmienia, jak to zmienie, wiec lepiej zrobic z prototype.
+
+
+
+//klasa osoba
+
+function Person (name, adress, id) {
+    this.name = name;
+    this.adress = adress;
+    this.id = id.toString();
+
+}
+
+Person.prototype.displayFullInfo = function (){
+
+    console.log('Imię: ' + this.name + ' Adres: ' + this.adress + 'Id: ' + this.id);
+
+}
+
+Person.prototype.ChangeId = function(newId) {
+
+    this.id = newId.toString();
+
+}
+
+//klasa student
+
+function Student(name, adress, id){
+
+Person.call(this, name, adress, id);
+this.role = "student"
+
+}
+
+//klasa nauczyciel
+
+function Teacher(name, adress, id){
+
+Person.call(this, name, adress, id);
+this.role = "teacher"
+
+}
+
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student;
+
+//konstruktorem jest student a nie person!
+
+Teacher.prototype = Object.create(Person.prototype);
+Teacher.prototype.constructor = Teacher;
+
+
+var students = [
+new Student("Kamila", "Gdansk", 2302),
+new Student("Ania", "Gdynia", 3445)
+]
+
+var teachers = [
+new Teacher("Andrzej", "Warszawa", 3403),
+new Teacher("Tomek", "Kraków", 6421)
+]
---
+++ indexday1.js
@@ -0,0 +1,163 @@
+class Person {
+    constructor(name, adress, id, age) {
+
+        this.name = name;
+        this.adress = adress;
+        this.id = id.toString();
+        this.age = age.toString();
+
+    }
+
+    //metoda jest dostepna w klasie, wiec jak dziedziczy to razem z metoda - mimo ze nie jest w konstruktorze, to nie ma to znaczenia - jest w klasie!
+    // metody robię poza konstruktorem!
+
+    changeId(newId) {
+
+        this.id = newId.toString();
+
+    }
+
+    displayFullInfo() {
+
+        console.log(`Imię: ${this.name} Adres: ${this.adress} Id: ${this.id} Wiek: ${this.age} Rola: ${this.role}`);
+
+    }
+
+}
+
+class Student extends Person {
+    constructor(name, adress, id, age) {
+
+        super(name, adress, id, age);
+        this.role = 'student';
+
+    }
+
+}
+
+class Teacher extends Person {
+    constructor(name, adress, id, age) {
+
+        super(name, adress, id, age);
+        this.role = 'nauczyciel';
+
+    }
+
+}
+
+
+const StudentsArray = [
+    new Student("Kamila", "Gdansk", 2302, 21),
+    new Student("Ania", "Gdynia", 3445, 22),
+    new Student("Jagoda", "Sopot", 2125, 23),
+    new Student("Martyna", "Wejherowo", 9885, 20),
+]
+
+
+const TeachersArray = [
+    new Teacher("Andrzej", "Warszawa", 3403, 34),
+    new Teacher("Tomek", "Kraków", 6421, 45),
+    new Teacher("Michał", "Wrocław", 9821, 29),
+    new Teacher("Maciek", "Poznań", 4421, 35)
+]
+
+
+
+
+class Render {
+
+    static renderText(arrayType, nameOfDiv) {
+
+        const o_cont = document.querySelector(nameOfDiv);
+        let o_html = '';
+        arrayType.map((element) => {
+            o_html += `<div><div>${element.name}</div><div>${element.adress}</div><div>${element.id}</div><div>${element.age}</div><div>${element.role}</div></div>`;
+        });
+
+        o_cont.innerHTML = o_html;
+    }
+}
+
+Render.renderText(StudentsArray, '#students');
+Render.renderText(TeachersArray, '#teachers');
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let inputName = document.querySelector('#inputName');
+// let inputCity = document.querySelector('#inputCity');
+// let inputId = document.querySelector('#inputId');
+// let inputAge = document.querySelector('#inputAge');
+// let inputRole = document.querySelector('#inputRole');
+// let button = document.querySelector("#addButton");
+
+// let Name = inputName.value;
+// let City = inputCity.value;
+// let Id = inputId.value;
+// let Age = inputAge.value;
+// let Role = inputRole.value;
+
+
+// let studentsContainer = document.querySelector(".students");
+// let teachersContainer = document.querySelector(".teachers");
+
+// button.onclick = function(){
+
+// if(Role === "nauczyciel"){
+
+// const nauczyciel = document.createElement('div');
+// nauczyciel.innerHTML = "blablabalabala"
+// teachersContainer.appendChild(nauczyciel);
+
+
+// }
+
+// if(Role === "student")
+
+// const student = document.createElement('div');
+// student.innerHTML = "blablabalabala"
+// studentsContainer.appendChild(student);
+
+// }
